d0ebcc1b16da197b2a2a8c1c23c29f6ef5080128
GGUKE <pllbong2020@naver.com>
2024-11-22 11:34:29 +0900

    2024-11-22

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './css2/style.css';
+import FetchAPI from './FetchAPI';
+
+
+function App(){
+	return(
+		<div className="container">
+			<FetchAPI />
+		</div>
+	);
+}
+
+export default App;
